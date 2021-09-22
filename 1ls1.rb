fdbd0119b16870b7212d34e5738357104a81095f
ReiTsukiko <90697384+reitsukiko@users.noreply.github.com>
2021-09-22 20:20:46 +0300

    Update 1ls1.rb

--- 1ls1.rb
+++ 1ls1.rb
@@ -3,9 +3,9 @@ name = gets.chomp
 
 puts "Hello, #{name}! I will help you find out ideal weight!"
 puts "How tall are you?"
-height = gets.chomp
+height = gets.chomp.to_i
 
-ideal_weight = ((height.to_i - 110) * 1.15).ceil
+ideal_weight = ((height - 110) * 1.15).ceil
 
 if ideal_weight >= 0
     puts "#{name}, your ideal weight #{ideal_weight}."
